2d668cd906c280698b4065dba4f09641250c8db4
JuShapiro <yuliashepel19@gmail.com>
2024-06-03 11:39:53 +0200

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,14 +1,17 @@
-import { InfinitySpin } from "react-loader-spinner";
+import { Oval } from "react-loader-spinner";
 import css from "./Loader.module.css";
 
 const Loader = () => {
   return (
     <div className={css.backdrop}>
-      <InfinitySpin
+      <Oval
         visible={true}
-        width="200"
-        color="#4fa94d"
-        ariaLabel="infinity-spin-loading"
+        height="80"
+        width="80"
+        color="grey"
+        ariaLabel="oval-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
       />
     </div>
   );
